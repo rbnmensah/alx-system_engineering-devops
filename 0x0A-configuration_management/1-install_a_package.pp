d07beca8df49f071d2023fa2a570ebab2cc32159
rbnmensah <ruebenmensah@yahoo.com>
2023-11-26 17:44:05 -0800

    magconfig

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,6 @@
 # install a flask
 
 package {'flask':
-	ensure => '2.1.0',
-	provider => 'pip3',
-	}
-
+ensure   => '2.1.0',
+provider => 'pip3',
+}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # kill process killmenow
 
 exec {'pkill':
-	command => 'pkill killmenow',
-	provider => 'shell',
-	}
-
+command  => 'pkill killmenow',
+provider => 'shell',
+}
