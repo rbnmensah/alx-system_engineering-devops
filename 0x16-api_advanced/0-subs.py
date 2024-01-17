75ba9436c2d9a7629a7b07374a6c369fb93d9336
Reuben <rbnmensah@gmail.com>
2024-01-17 01:52:38 +0000

    api_ad

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,7 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json" .format(subreddit), headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
 
