9172087fdc5eb7e34520190f8138f3d47b4de566
Reuben Mensah <rbnmensah@gmail.com>
2024-01-18 02:30:30 +0000

    webstack17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-#fixes phpp to php
-
-exec{'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path 	=> '/usr/local/bin/:/bin/'
+# Fixes a wordpress site running on apache2
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
